c2b01f1c5e55a81115122d798dcdcb8b13e4ea90
Nerina Grebovic <nerinagrebovic12@gmail.com>
2025-05-14 13:16:52 +0200

    povezan backend i frontend login-a

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setErrorMsg('');
+
+    try {
+      const response = await axios.post('http://localhost:8000/users/login', {
+        email,
+        password,
+      });
+
+      const { access_token } = response.data;
+      console.log(access_token)
+
+      // Čuvanje tokena lokalno
+      localStorage.setItem('token', access_token);
+
+      // Redirect ili neka druga akcija
+      //window.location.href = '/dashboard';
+    } catch (err) {
+      console.error('Login error:', err);
+      setErrorMsg('Pogrešan email ili lozinka.');
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-card">
@@ -10,14 +39,28 @@ function Login() {
         </div>
         <div className="login-right">
           <h2>Prijava</h2>
-          <form>
-            <input type="email" placeholder="Email adresa" required />
-            <input type="password" placeholder="Lozinka" required />
+          <form onSubmit={handleLogin}>
+            <input
+              type="email"
+              placeholder="Email adresa"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Lozinka"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
             <button type="submit">Prijavi se</button>
           </form>
+
+          {errorMsg && <p className="error-msg">{errorMsg}</p>}
+
           <div className="login-links">
             <a href="#">Zaboravili ste lozinku?</a>
-        
           </div>
         </div>
       </div>
