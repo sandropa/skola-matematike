a6de7275f294d03d678ca18ed33d2193d4326313
Ramina22 <162555274+Ramina22@users.noreply.github.com>
2025-05-21 13:50:49 +0200

    CRUD for users

--- server/schemas/user.py
+++ server/schemas/user.py
@@ -4,14 +4,24 @@ from pydantic import BaseModel, EmailStr
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+    name: str
+    surname: str
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+class UserUpdate(BaseModel):
+    email: EmailStr
+    password: str
+    name: str
+    surname: str
+
 class UserOut(BaseModel):
     id: int
     email: EmailStr
+    name: str
+    surname: str
 
     class Config:
         orm_mode = True
