fc6ebe15bb271d2f23a06accadc6c8542f37ab54
sandropa <sandro.paradzik@gmail.com>
2025-06-08 23:44:38 +0200

    also translate to bosnian (it is more cool)

--- server/services/prompts.py
+++ server/services/prompts.py
@@ -4,7 +4,7 @@ SYSTEM_PROMPTS = {
     "translate": "You are a helpful assistant that translates math problems from English to Bosnian.",
     "fix_latex": "You are a LaTeX expert who fixes malformed LaTeX code. Don't put the code inside '```'. Don't try to solve the problems, just return everything the same except for fixed latex code.",
     "hello": "You are a friendly assistant that responds with a JSON greeting message.",
-    "extract_latex_from_image": "You are a latex extraction engine. You recive an image of math problems and return latex code. Don't put the code inside '```'."
+    "extract_latex_from_image": "You are a latex extraction engine, and translation to Bosnian engine. You recive an image of math problems and return latex code. Don't put the code inside '```'. You need to translate given math problems to bosnian, and use appropriate Bosnian math terminology. You return ONLY the translation."
 }
 
 HELLO_EXAMPLES: list[tuple[str, str]] = [
