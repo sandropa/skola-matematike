6a24532642b52e2477560b6f52982f158c28d8c1
Nerina Grebovic <nerinagrebovic12@gmail.com>
2025-06-14 11:01:02 +0200

    frontend google

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'; // <-- Import
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <-- Wrap App */}
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="372680745111-b0vfb2ee3du7h0qg2a0eii9vreg4gsjs.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
-)
+);
